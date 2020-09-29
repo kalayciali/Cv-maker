8e52e01a3a29450cbbe75ea229c84bb744411571
phymat <kalayciali857@gmail.com>
2020-09-29 18:55:28 +0300

    add profile sign up styles

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
 from django.forms import ModelForm
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Row, Column
 
 class SignUpForm(UserCreationForm):
     profile_pic = forms.ImageField(label="Profil fotoğrafınız")
@@ -19,6 +21,39 @@ class SignUpForm(UserCreationForm):
         'placeholder': """www.linkedin.com/in/username/
 www.twitter.com/username""",
     }) )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Row(
+                Column('profile_pic', css_class='form-group col-md-4 mb-0'),
+            ),
+            Row(
+                Column('first_name', css_class='form-group col-md-3 mb-0'),
+                Column('last_name', css_class='form-group col-md-3 mb-0'),
+            ),
+            Row(
+                Column('email', css_class='form-group col-md-3 mb-0'),
+                Column('username', css_class='form-group col-md-3 mb-0'),
+            ),
+            Row(
+                Column('password1', css_class='form-group col-md-3 mb-0'),
+                Column('password2', css_class='form-group col-md-3 mb-0'),
+            ),
+            Row(
+                Column('address', css_class='form-group col-md-3 mb-0'),
+                Column('phone_num', css_class='form-group col-md-3 mb-0'),
+            ),
+            Row(
+                Column('links', css_class='form-group col-md-3 mb-0'),
+                Column('bullet_descript', css_class='form-group col-md-3 mb-0'),
+            ),
+            Row(
+                Column('descript', css_class='form-group col-md-4 mb-0'),
+            ),
+            Submit('submit', 'Kaydol', css_class='btn btn-dark')
+        )
 
     class Meta():
         model = User
